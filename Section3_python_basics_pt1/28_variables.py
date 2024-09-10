04c55c2e2781a9dfe21b71977142a1ac5a0e09c8
Yavor Chanov <yavorchanov@gmail.com>
2024-09-10 22:19:40 +0300

    add lection 28 and 38

---
+++ Section3_python_basics_pt1/28_variables.py
@@ -0,0 +1,11 @@
+# variables
+# use snake case (my_variable)
+# start with lower case, don't start with numbers
+# don't override reserved funcitons and keywords
+
+user_IQ = 190
+
+print(user_IQ)
+
+# constants - they are capitalized and they state 'dont change this' (you still can do it)
+PI = 3.14
---
+++ Section3_python_basics_pt1/38_build_in_methods.py
@@ -0,0 +1,6 @@
+quote = 'to be or not to be'
+
+print(quote.upper())
+print(quote.find('be'))
+print(quote.replace('be', 'me'))
+print(quote) #does not rewrite the variable
