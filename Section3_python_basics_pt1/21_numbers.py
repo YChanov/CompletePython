6229f489d7c3bd333f9ecf370b99d1df5a72ed3f
Yavor Chanov <yavorchanov@gmail.com>
2024-09-10 07:49:12 +0300

    added section3, lection 21 and 22

---
+++ Section3_python_basics_pt1/21_numbers.py
@@ -0,0 +1,13 @@
+#Fundamental data types
+# int and float
+print(type(2+4))
+print(type(2 - 4))
+print(type(2 * 12))
+
+print(type(2 / 4))
+print(type(20 + 1.1))
+print(type(20 + 12))
+
+print(type(2 ** 6))
+print(type(2 // 4))
+print(type(5 % 4))
---
+++ Section3_python_basics_pt1/22_math_funcitons.py
@@ -0,0 +1,4 @@
+# Math functions
+# Here we have just an example. We can always search what kind of math functions we have in our hands with python in internet
+print(round(3.1))
+print(abs(-20))
