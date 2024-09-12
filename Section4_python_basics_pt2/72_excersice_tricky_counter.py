1b7bfc18931fb4c3fc6f2a08b45e0234224beea7
Yavor Chanov <yavorchanov@gmail.com>
2024-09-12 09:05:37 +0300

    add lections 72 and 74

---
+++ Section4_python_basics_pt2/72_excersice_tricky_counter.py
@@ -0,0 +1,8 @@
+# sum the values in a list
+my_list = [1,2,3,4,5,6,7,8,9,10]
+
+summed_value = 0
+for number in my_list:
+    summed_value += number
+
+print(summed_value)
---
+++ Section4_python_basics_pt2/74_enumerate.py
@@ -0,0 +1,3 @@
+for i, item in enumerate(list(range(0, 100, 2))):
+    if item < 50:
+        print("The index of value {} is {}".format(item, i))
