8d95100e6cb88ddbb1f7903bf1d1aa8951797355
Yavor Chanov <yavorchanov@gmail.com>
2024-10-29 00:18:43 +0200

    add leciton 172

---
+++ Section11_modules/172_packages/main.py
@@ -0,0 +1,5 @@
+import utility
+from shopping import shopping_cart
+
+print(shopping_cart.buy('apple'))
+print(utility.multiply(2,3))
--- Section11_modules/main.py
+++
@@ -1,4 +0,0 @@
-import utility
-from Section11_modules.utility import multiply
-
-print(multiply(2,5))
